c980cf118ca2e1fa4e208ddb31da672527c1a554
Jorge Harbes <jharbes@hotmail.com>
2022-09-13 17:15:08 -0300

    mais exercicios

---
+++ lista-exercicios-2-javascript/ex05-retornoPrimeiroMaior.js
@@ -0,0 +1,7 @@
+const primeiroMaior=(primeiro,segundo)=>{
+    return primeiro>=segundo?true:false 
+}
+
+console.log(primeiroMaior(0,0))
+console.log(primeiroMaior(5,1))
+console.log(primeiroMaior(5,16))
