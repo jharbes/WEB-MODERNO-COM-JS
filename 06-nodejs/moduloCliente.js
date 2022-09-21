bd0d7cf5da8ff44100842f709b50b3df2809fe21
Jorge Harbes <jharbes@hotmail.com>
2022-09-21 12:59:27 -0300

    Update moduloCliente.js

--- 06-nodejs/moduloCliente.js
+++ 06-nodejs/moduloCliente.js
@@ -1,7 +1,11 @@
-const moduloA=require('./moduloA')
+const moduloA=require('./moduloA') // usar a extensão .js no nome do arquivo é opcional
 const moduloB=require('./moduloB')
 
 console.log(moduloA.ola)
 console.log(moduloA.bemVindo)
 console.log(moduloA.ateLogo)
 console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
