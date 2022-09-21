ab47020a8287a21189f5ffcdb943ff8da2529d2d
Jorge Harbes <jharbes@hotmail.com>
2022-09-21 12:53:45 -0300

    modulos

--- 06-nodejs/moduloA.js
+++ 06-nodejs/moduloA.js
@@ -1,3 +1,8 @@
+console.log(this)
+
 this.ola='Fala Pessoal'
 exports.bemVindo='Bem vindo ao node!'
 module.exports.ateLogo='Até o próximo exemplo!'
+
+console.log(this)
+console.log(module.exports)
