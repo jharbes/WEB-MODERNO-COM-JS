a3ccb7ebc5c747f9880603754b899185ad903408
Jorge Harbes <jharbes@hotmail.com>
2022-09-29 14:24:25 -0300

    Update servidor.js

--- 06-nodejs/projeto/src/servidor.js
+++ 06-nodejs/projeto/src/servidor.js
@@ -4,6 +4,11 @@ const express=require('express')
 const app=express()
 
 app.get('/produtos',(req,res,next)=>{
+    console.log('Middleware 1...')
+    next()
+})
+
+app.get('/produtos',(req,res,next)=>{
     res.send({nome:'Notebook',preco:123.45}) // Converter para JSON
 })
 
