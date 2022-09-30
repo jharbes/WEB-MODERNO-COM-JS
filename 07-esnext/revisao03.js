11a6b21607d64c71bbe07af7053e0e94585910c6
Jorge Harbes <jharbes@hotmail.com>
2022-09-30 06:35:10 -0300

    Create revisao03.js

---
+++ 07-esnext/revisao03.js
@@ -0,0 +1,13 @@
+// ES8: Object.values/Object.entries    
+const obj={a:1,b:2,c:3}
+console.log(Object.values(obj)) // Retornará um array com os valores dos objetos em formato de array
+
+console.log(Object.keys(obj)) // Retornará um array com as chaves do objeto em formato de array
+
+console.log(Object.entries(obj)) // Retornará o par chave/valor em formato de array, onde cada elemento do array será um array com cada um dos pares chave/valor
+
+// Melhorias na notação literal
+const nome='Carla'
+const pessoa={
+    
+}
