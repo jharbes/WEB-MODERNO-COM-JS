e2518f0d9a87742c0a63525866a7fa0c0e054339
Jorge Harbes <jharbes@hotmail.com>
2022-11-16 15:24:12 -0300

    Update consultarEstados.sql

--- 28-banco-relacional/consultarEstados.sql
+++ 28-banco-relacional/consultarEstados.sql
@@ -1,1 +1,8 @@
 select * from estados;
+
+select nome,sigla from estados;
+
+select sigla,nome as 'Nome do Estado' from estados; -- altera o nome para 'Nome do Estado' na saida dos dados
+
+select sigla,nome as 'Nome do Estado' from estados -- altera o nome bem como filtra apenas estados da regiao sul
+where regiao='Sul';
