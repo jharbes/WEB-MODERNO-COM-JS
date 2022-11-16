f75ef1939afa461795622711c3bf0f5c7f6d4e46
Jorge Harbes <jharbes@hotmail.com>
2022-11-16 15:03:31 -0300

    alt nome arquivo

--- 28-banco-relacional/inserirEstados2.sql
+++ 28-banco-relacional/inserirEstados2.sql
@@ -26,14 +26,3 @@ VALUES
     ('Sergipe', 'SE', 'Nordeste', 2.29),
     ('Tocantins', 'TO', 'Norte', 1.55);
 
--- Exemplo de Insert 1
-INSERT INTO estados (nome, sigla, regiao, populacao)
-    VALUES ('Acre', 'AC', 'Norte', 0.83);
-
--- Exemplo de Insert 2
-INSERT INTO estados 
-    (nome, sigla, regiao, populacao)
-VALUES
-    ('Alagoas', 'AL', 'Nordeste', 3.38),
-    ('Amapá', 'AP', 'Norte', 0.8),
-    ('Amazonas', 'AM', 'Norte', 4.06);
