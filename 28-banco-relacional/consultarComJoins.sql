1b6d8d3f09d7951ab2202fe20cc5475ac2e82584
Jorge Harbes <jharbes@hotmail.com>
2022-11-17 14:01:45 -0300

    Update consultarComJoins.sql

--- 28-banco-relacional/consultarComJoins.sql
+++ 28-banco-relacional/consultarComJoins.sql
@@ -1,1 +1,12 @@
+select * from prefeitos;
+
+select * from `cidades`;
+
+/*
+Os comandos presentes nesse arquivo foram utilizados no workbench pois o retorno
+gerado no VSCode não estava de acordo com o esperado.
+*/
+
 select * from `cidades` c inner join prefeitos p on c.id=p.cidade_id;
+
+select * from `cidades` c left join prefeitos p on c.id=p.cidade_id;
