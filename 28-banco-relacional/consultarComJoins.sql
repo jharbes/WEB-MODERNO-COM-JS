aa1687976213a81cacccdbbd35166564eeb89cde
Jorge Harbes <jharbes@hotmail.com>
2022-11-17 14:10:24 -0300

    Update consultarComJoins.sql

--- 28-banco-relacional/consultarComJoins.sql
+++ 28-banco-relacional/consultarComJoins.sql
@@ -10,3 +10,11 @@ gerado no VSCode não estava de acordo com o esperado.
 select * from `cidades` c inner join prefeitos p on c.id=p.cidade_id;
 
 select * from `cidades` c left join prefeitos p on c.id=p.cidade_id;
+
+select * from `cidades` c left outer join prefeitos p on c.id=p.cidade_id; -- exatamente igual ao comando anterior
+
+select * from `cidades` c right join prefeitos p on c.id=p.cidade_id;
+
+select * from `cidades` c left join prefeitos p on c.id=p.cidade_id
+union
+select * from `cidades` c right join prefeitos p on c.id=p.cidade_id; -- no caso desse comando estamos emulando o full join que nao existe nativamente no mysql
