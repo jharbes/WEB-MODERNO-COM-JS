2330c553c7e221dcd81f1d0d9a8f3e52c81c2d21
Jorge Harbes <jharbes@hotmail.com>
2022-11-17 12:50:32 -0300

    Create inserirPrefeitos.sql

---
+++ 28-banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,10 @@
+select * from `cidades`;
+
+SELECT * FROM `prefeitos`;
+
+insert into prefeitos
+    (nome,cidade_id)
+values
+    ('Rodrigo Neves',2),
+    ('Raquel Lyra',3),
+    ('Zenaldo Coutinho',null);
